591fe1a64bb3a74ce6566e6df52d6c02876333f2
fyxiao <nbtv@foxmail.com>
2018-12-12 10:22:10 +0800

    新增绑定买号接口

--- src/containers/myCenter/buyAdmin/buyAdmin.js
+++ src/containers/myCenter/buyAdmin/buyAdmin.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Icon } from 'antd';
+import axios from 'axios';    //ajax
+
+const token = localStorage.getItem("token");
 
 class BuyAdmins extends Component {
   constructor() {
     super();
     this.state = {
-      taobao: true,
+      taobao: false,
       pinduoduo: false,
       jingdong: false
     }
   }
+
+  componentWillMount() {
+    // 在此调用ajax 获取绑定买号列表
+    axios.get('/api/index/tbBind',{headers: {AppAuthorization: token}})   //传入唯一标识
+    .then(response => {
+      console.log(response.data.data);
+      // 获取绑定买号数据
+      this.setState({
+        jd_bind: response.data.data.jd_bind.nickname,                 //京东用户名
+        jd_status: response.data.data.jd_bind.bind_status,            //京东绑定状态
+        pdd_bind: response.data.data.pdd_bind.nickname,               //拼多多用户名
+        pdd_status: response.data.data.pdd_bind.bind_status,          //拼多多绑定状态
+        taobao_bind: response.data.data.taobao_bind.nickname,         //淘宝用户名
+        taobao_status: response.data.data.taobao_bind.bind_status,    //淘宝绑定状态
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
   render() {
-    // const { taobao, pinduoduo, jingdong } = this.state;
+    const { jd_bind, jd_status, pdd_bind, pdd_status, taobao_bind, taobao_status } = this.state;
     return(
       <div>
         <header className="tabTitle">
@@ -25,30 +49,30 @@ class BuyAdmins extends Component {
             <li>
               <Link to="/taobao">
                 <div className="bind-list">
-                  <div><img src={require("../../../img/taobao.png")} alt="淘宝图标"/><span>绑定淘宝账号</span></div>
-                  <div><span>未绑定</span><img src={require("../../../img/jinru.png")} alt="进入"/></div>
+                  <div><img src={require("../../../img/taobao.png")} alt="淘宝图标"/><span>{ taobao_bind }</span></div>
+                  <div><span>{ taobao_status }</span><img src={require("../../../img/jinru.png")} alt="进入"/></div>
                 </div>
-                <p className="bind-content">未通过</p>
+                <p className="bind-content"></p>
               </Link>
             </li>
             {/* 绑定拼多多账号 */}
             <li>
               <Link to="/pinduoduo">
                 <div className="bind-list">
-                  <div><img src={require("../../../img/pinduoduo.png")} alt="拼多多图标"/><span>绑定拼多多账号</span></div>
-                  <div><span>未绑定</span><img src={require("../../../img/jinru.png")} alt="进入"/></div>
+                  <div><img src={require("../../../img/pinduoduo.png")} alt="拼多多图标"/><span>{ pdd_bind }</span></div>
+                  <div><span>{ pdd_status }</span><img src={require("../../../img/jinru.png")} alt="进入"/></div>
                 </div>
-                {/* <p className="bind-content">123123</p> */}
+                <p className="bind-content"></p>
               </Link>
             </li>
             {/* 绑定京东账号 */}
             <li>
               <Link to="/jingdong">
                 <div className="bind-list">
-                  <div><img src={require("../../../img/jingdong1.png")} alt="京东图标"/><span>绑定京东账号</span></div>
-                  <div><span>未绑定</span><img src={require("../../../img/jinru.png")} alt="进入"/></div>
+                  <div><img src={require("../../../img/jingdong1.png")} alt="京东图标"/><span>{ jd_bind }</span></div>
+                  <div><span>{ jd_status }</span><img src={require("../../../img/jinru.png")} alt="进入"/></div>
                 </div>
-                {/* <p className="bind-content">123123</p> */}
+                <p className="bind-content"></p>
               </Link>
             </li>
           </ul>
--- src/containers/myCenter/buyAdmin/taobao/taobao.js
+++ src/containers/myCenter/buyAdmin/taobao/taobao.js
@@ -1,15 +1,17 @@
 import React,{ Component } from 'react';
 import { Icon, Form, Input, Button, Cascader, Modal, message  } from 'antd';
 import { Link } from 'react-router-dom';
-// import { ImagePicker } from 'antd-mobile';
+import axios from 'axios';
 import ImagePicker from 'antd-mobile/lib/image-picker';
 
 import '../buyAdmin.css';
 
+const token = localStorage.getItem("token");
 const city_new = require('../../../../component/city.js');    //三级联动资源库
 const data = [];
 const FormItem = Form.Item;
 const options = city_new.data.RECORDS;    //展示用户选择的省市区
+const phoneNum = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/;   //手机号码正则
 message.config({
   top: 300,
 });
@@ -66,11 +68,48 @@ class BindTaobaos extends Component {
   // 数据提交、ajax交互
   handleSubmit = (e) => {
     e.preventDefault();
-    let _this = this.state.files
+    let this_ = this;
+    let _this = this.state.files    //用户上传图片集合
     this.props.form.validateFields((err, values) => {
       if ( !err === true && _this.length >= 2 ) {
-        console.log(values);
-        message.success('提交成功，待审核！');
+        // 所有数据填写完毕后 进入下一阶段判断
+        if ( !phoneNum.test(values.GoodsPhone) ) {
+          message.error("请输入正确的手机号码！")
+        } else {
+          // console.log(values);
+          // 图片集合存入imgs 传给后端
+          let imgs = [values.images[0].url, values.images[1].url]
+          // console.log(imgs);
+          //以上数据都正确 在此 ajax交互
+          axios.post('/api/index/tbOperate',
+          {
+            Account: values.Account,                  //绑定帐号昵称
+            GoodsName: values.GoodsName,              //收货人
+            address: values.address,                  //收货人地址
+            GoodsPhone: values.GoodsPhone,            //收货人手机号
+            tb_order_sign: values.tb_order_sign,      //最近淘宝下单订单号
+            provinces: values.provinces,              //省市区组合
+            AlipayName: values.AlipayName,            //支付宝姓名
+            images: imgs,                             //图片集合
+          },
+          {
+            headers: {AppAuthorization: token}    //post 方法传 token
+          })
+          .then(function (response) {   //调用接口成功执行
+            let data_ = response.data;
+            if ( data_.status ) {
+              message.success(data_.msg);
+              this_.props.history.push("/buyAdmin")
+            } else {
+              message.warning(data_.msg);
+              this_.props.history.push("/buyAdmin")
+            }
+            console.log(data_);
+          })
+          .catch(function (error) {   //调用接口失败执行
+            console.log(error);
+          });
+        }
       }else {
         message.error('请完善信息');
       }
@@ -92,7 +131,7 @@ class BindTaobaos extends Component {
             <FormItem
               label="淘宝旺旺账号："
             >
-              {getFieldDecorator('note1', {
+              {getFieldDecorator('Account', {
                 rules: [{ required: true, message: '请输入正确的淘宝旺旺账号：!' }],
               })(
                 <Input className="buy-input" placeholder="旺旺账号" />
@@ -101,16 +140,16 @@ class BindTaobaos extends Component {
             <FormItem
               label="最近的淘宝订单号："
             >
-              {getFieldDecorator('note2', {
+              {getFieldDecorator('tb_order_sign', {
                 rules: [{ required: true, message: '请输入正确的淘宝订单号!' }],
               })(
-                <Input className="buy-input" placeholder="淘宝订单号" />
+                <Input className="buy-input" maxLength="18" placeholder="淘宝订单号" />
               )}
             </FormItem>
             <FormItem
               label="收货人姓名："
             >
-              {getFieldDecorator('note3', {
+              {getFieldDecorator('GoodsName', {
                 rules: [{ required: true, message: '请输入收货人姓名!' }],
               })(
                 <Input className="buy-input" placeholder="姓名" />
@@ -118,7 +157,7 @@ class BindTaobaos extends Component {
             </FormItem>
             <FormItem label="所在地区：">
               {/* <Cascader options={options} onChange={this.onChange} placeholder="Please select" /> */}
-              {getFieldDecorator('note4', {
+              {getFieldDecorator('provinces', {
                 rules: [{ required: true, message: '请完全所在地区!' }],
               })(
                 <Cascader options={options} onChange={this.onChange} placeholder="所在地区" />
@@ -127,7 +166,7 @@ class BindTaobaos extends Component {
             <FormItem
               label="详细地址"
             >
-              {getFieldDecorator('note5', {
+              {getFieldDecorator('address', {
                 rules: [{ required: true, message: '请输入详细地址!' }],
               })(
                 <Input className="buy-input" placeholder="详细地址" />
@@ -136,7 +175,7 @@ class BindTaobaos extends Component {
             <FormItem
               label="收货人手机"
             >
-              {getFieldDecorator('note6', {
+              {getFieldDecorator('GoodsPhone', {
                 rules: [{ required: true, message: '请输入收货人手机号码!' }],
               })(
                 <Input className="buy-input" type="text" maxLength="11" placeholder="收货人手机号" />
@@ -145,7 +184,7 @@ class BindTaobaos extends Component {
             <FormItem
               label="支付宝姓名"
             >
-              {getFieldDecorator('note7', {
+              {getFieldDecorator('AlipayName', {
                 rules: [{ required: true, message: '请输入支付宝姓名!' }],
               })(
                 <Input className="buy-input" placeholder="支付宝姓名" />
@@ -154,7 +193,7 @@ class BindTaobaos extends Component {
             <FormItem
               label="上传 我的淘宝 和 支付宝实名截图"
             >
-              {getFieldDecorator('note8', {
+              {getFieldDecorator('images', {
                 rules: [{ required: true, message: '请上传淘宝和支付宝截图!' }],
               })(
                 <ImagePicker
