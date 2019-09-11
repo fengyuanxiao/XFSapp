ce2450f7b0e02a3e82f0f6c47cb4ba9059d26453
fyxiao <nbtv@foxmail.com>
2019-09-11 10:08:34 +0800

    添加幸福树build

---
+++ build/precache-manifest.140ad881efe9c646be2e4d878f53e290.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7706175bbab21f41af6e15ddded114a",
+    "url": "./static/media/1-wap.b7706175.jpg"
+  },
+  {
+    "revision": "131e131befb2ab64ac43",
+    "url": "./static/css/main.d3389b90.chunk.css"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4988848afacf4f2eb562a9be86b888f5",
+    "url": "./static/media/headerImg.4988848a.png"
+  },
+  {
+    "revision": "9cadc121128cbb0c4452",
+    "url": "./static/js/2.15f22f72.chunk.js"
+  },
+  {
+    "revision": "c0307fd860f57e416e5023cd1319b2ff",
+    "url": "./static/media/loading.c0307fd8.gif"
+  },
+  {
+    "revision": "bf10852163032b68c28205555f6ccd79",
+    "url": "./static/media/mytaobao.bf108521.png"
+  },
+  {
+    "revision": "f3fbb9b45c4748ea8e2a98b07b21fe12",
+    "url": "./static/media/myzhifubao.f3fbb9b4.png"
+  },
+  {
+    "revision": "4da8f74886ccea5828f03ac2f3d76670",
+    "url": "./static/media/tbdingdan.4da8f748.png"
+  },
+  {
+    "revision": "782f19442a76bfb050e27b46f75ae945",
+    "url": "./static/media/jd1.782f1944.png"
+  },
+  {
+    "revision": "b2cc653d617398cf6a10b06df81f9ee4",
+    "url": "./static/media/jd2.b2cc653d.png"
+  },
+  {
+    "revision": "729ed74dd6c64eccea47c33cddac147e",
+    "url": "./static/media/jd3.729ed74d.png"
+  },
+  {
+    "revision": "2f30488a5ea9028fdb67e0a52cdf4602",
+    "url": "./static/media/plus.2f30488a.png"
+  },
+  {
+    "revision": "4a7e68114aa10b846c3769cb99898b9d",
+    "url": "./static/media/wykl3.4a7e6811.jpg"
+  },
+  {
+    "revision": "822b69bccf242d95f1e5ce10d59cacef",
+    "url": "./static/media/wykl1.822b69bc.jpg"
+  },
+  {
+    "revision": "fea756587d4d477ac80383c64a60076f",
+    "url": "./static/media/wykl2.fea75658.jpg"
+  },
+  {
+    "revision": "9e55551aec321a575a2191581e7a29e7",
+    "url": "./static/media/hongbao.9e55551a.png"
+  },
+  {
+    "revision": "131e131befb2ab64ac43",
+    "url": "./static/js/main.61a40c97.chunk.js"
+  },
+  {
+    "revision": "cfe81701caa93e8304bee08e86625c53",
+    "url": "./static/media/activity.cfe81701.png"
+  },
+  {
+    "revision": "481fd8578d9a99e366c715b430846c70",
+    "url": "./static/media/wenda.481fd857.png"
+  },
+  {
+    "revision": "711d2fee028b8bd584a0231a1551ade1",
+    "url": "./static/media/4444.711d2fee.jpg"
+  },
+  {
+    "revision": "3bddb08ce16a9b17c08e7bf928e60544",
+    "url": "./static/media/5555.3bddb08c.jpg"
+  },
+  {
+    "revision": "86b951b7e1a99c4476504b8f9eb976e3",
+    "url": "./static/media/6666.86b951b7.jpg"
+  },
+  {
+    "revision": "c4edd7b3d0c51306bf1c450b7420594d",
+    "url": "./static/media/jdshili.c4edd7b3.jpg"
+  },
+  {
+    "revision": "f7d2260d1bf4f3a3087f463bc3648da0",
+    "url": "./static/media/pddorder.f7d2260d.png"
+  },
+  {
+    "revision": "3ae2b1bbd35ac1a2cb5c793c7fcdef02",
+    "url": "./static/media/wphorder.3ae2b1bb.jpg"
+  },
+  {
+    "revision": "8bb077c416ba0bfd32fb6b81872f473b",
+    "url": "./static/media/guanjianci.8bb077c4.png"
+  },
+  {
+    "revision": "9e2404136232fd788471985aca8b2227",
+    "url": "./static/media/pinduoduo1.9e240413.png"
+  },
+  {
+    "revision": "aca191d8c2cb106c57bff559d2a54bc2",
+    "url": "./static/media/pinduoduo2.aca191d8.png"
+  },
+  {
+    "revision": "53808034fb9b2bc2e054f26e2610b375",
+    "url": "./static/media/wph1.53808034.jpg"
+  },
+  {
+    "revision": "9ba416ad4fc8269fe9714733c24cfcd4",
+    "url": "./static/media/wph2.9ba416ad.jpg"
+  },
+  {
+    "revision": "2b306733e31cce0239c8517921d9f2b3",
+    "url": "./static/media/shiming.2b306733.jpg"
+  },
+  {
+    "revision": "9cadc121128cbb0c4452",
+    "url": "./static/css/2.d458c30f.chunk.css"
+  },
+  {
+    "revision": "83401b1f118fe2419dd7d888d30ce25c",
+    "url": "./index.html"
+  }
+];
